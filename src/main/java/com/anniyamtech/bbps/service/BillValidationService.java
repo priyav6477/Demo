ba64e2ccfdce2faea5e06d27313ac8245da72853
priyav6477 <69576111+priyav6477@users.noreply.github.com>
2022-08-15 17:34:23 +0530

    Delete src directory

--- src/main/java/com/anniyamtech/bbps/service/BillValidationService.java
+++
@@ -1,9 +0,0 @@
-package com.anniyamtech.bbps.service;
-
-import com.anniyamtech.bbps.config.BaseDTO;
-import com.anniyamtech.bbps.requestDto.BillValidationRequest;
-
-public interface BillValidationService {
-	public BaseDTO billValidation(BillValidationRequest billValidationRequest) throws Exception;
-
-}
--- src/main/java/com/anniyamtech/bbps/service/CheckComplaintStatusService.java
+++
@@ -1,9 +0,0 @@
-package com.anniyamtech.bbps.service;
-
-import com.anniyamtech.bbps.config.BaseDTO;
-import com.anniyamtech.bbps.requestDto.CheckComplaintStatusReqDto;
-
-public interface CheckComplaintStatusService {
-	public BaseDTO checkingComplaintStatus(CheckComplaintStatusReqDto checkComplaintReqDto);
-	
-}
